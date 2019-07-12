4e472010826e00a98842a1b74088d2b3d72fe8e3
busliming <471104091@qq.com>
2019-07-13 00:20:13 +0800

    移动端开发

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+---------------------------------------------------------------------------
+[移动端]  课程链接: https://www.3mooc.com/front/couinfo/830
+[swiper] 课程链接: https://www.3mooc.com/front/couinfo/834
+---------------------------------------------------------------------------
+*/
+var mySwiper = new Swiper ('.swiper-container', {
+ 
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination'
+  }
+  
+})        
